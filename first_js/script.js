0800f385d6636083317cb1345c87e2223b5b743a
Roman Karki <romankarki2017@gmail.com>
2026-01-14 09:18:41 +0545

    added  function

--- first_js/script.js
+++ first_js/script.js
@@ -4,3 +4,18 @@ var firstName = "Roman";
 var lastName = "Karki";
 
 alert("Full Name: " + firstName + " " + lastName);
+
+function scrpitLoader() {
+    alert('Script loaded');
+}
+function addition() {
+    var num1 = 5;
+    var num2 = 6;
+
+    var num3 = num1 + num2;
+
+    alert(num3);
+
+
+}
+addition();
