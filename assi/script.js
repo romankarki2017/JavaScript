0935e6750149e8efdeda6c0683fad58e20fa62c5
Roman Karki <romankarki2017@gmail.com>
2026-01-19 20:17:20 +0545

    used function

---
+++ assi/script.js
@@ -0,0 +1,13 @@
+// Concatenate first name and last name
+function getFullName(firstName, lastName) {
+  return firstName + " " + lastName;
+}
+// Basic function that says hello
+function sayHello() {
+  alert("Hello!");
+}
+
+var fullName = getFullName("Roman", "Karki");
+alert("Full Name: " + fullName);
+
+sayHello();
